62af5d2325b37c1956dc12b2102fb1ad658ad70a
Ich-Asadullah <ichasadullah@gmail.com>
2024-08-18 13:54:18 +0500

    Updated the streamlit gui

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from dotenv import load_dotenv
 from document_analyzer import get_pdf_page_count, analyze_document as analyze_document_azure
 from openai_analyzer import process_pdf, process_image
+from antropic_analyzer import process_pdf as antropic_pdf_analyzer
+from antropic_analyzer import process_image as antropic_image_analyzer
 import asyncio
 
 load_dotenv()
@@ -48,10 +50,20 @@ def process_file_azure(file, file_type):
         cost = 0.001
     st.write(f"Total cost: {cost}")
 
+async def process_file_antropic(file, file_type):
+    if file_type == "pdf":
+        results, total_cost, inp_tokens, res_tokens = await antropic_pdf_analyzer(file)
+        st.markdown("\n".join(results))
+    else:  # image
+        result, total_cost, inp_tokens, res_tokens = await antropic_image_analyzer(file)
+        st.markdown(result)
+    st.write(f"Total cost: ${total_cost:.6f}")
+
+
 def main():
     st.title("Document Analyzer")
     
-    analyzer_option = st.radio("Select Analyzer", ("Azure", "OpenAI"))
+    analyzer_option = st.radio("Select Analyzer", ("Azure", "OpenAI", "Antropic Image"))
     
     uploaded_file = st.file_uploader("Choose a file", type=["pdf", "jpg", "jpeg", "png"])
     
@@ -59,14 +71,16 @@ def main():
         file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type, "FileSize": uploaded_file.size}
         st.write(file_details)
         
-        file_type = "pdf" if uploaded_file.type == "application/pdf" else "image"
+        file_type = "pdf" if uploaded_file.type == "application/pdf" else f"image/{uploaded_file.name.split('.')[-1]}"
         
         if st.button("Analyze Document"):
             with st.spinner("Analyzing document..."):
                 if analyzer_option == "Azure":
                     process_file_azure(uploaded_file, file_type)
-                else:  # OpenAI
+                elif analyzer_option == "OpenAI":  # OpenAI
                     asyncio.run(process_file_openai(uploaded_file, file_type))
+                else:
+                    asyncio.run(process_file_antropic(uploaded_file, file_type))
 
 if __name__ == "__main__":
     if not st.session_state.logged_in:
